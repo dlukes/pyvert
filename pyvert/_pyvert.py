9091f2852326b5a2c3f7e3750a97c7716ba07ada
David Lukes <dafydd.lukes@gmail.com>
2016-05-31 17:06:56 +0200

    refactor _pyvert lib
    - especially the way valid struct names are detected
      - much more efficient now
      - the fastest option is to specify valid structs in the call to
      - ``iterstruct``
    - fix some inconsistencies in trailing newlines (which matter,
      unfortunately, because verticals are not real XML, so we can't fully
      rely on lxml for validity)
    - rework and extend fallback IDs

--- pyvert/_pyvert.py
+++ pyvert/_pyvert.py
@@ -1,39 +1,31 @@
+from itertools import chain
 from lazy import lazy
 from tempfile import NamedTemporaryFile as NamedTempFile
 
-import re
+import regex as re
 import random
 from lxml import etree
 from html import unescape
 
-__all__ = ["Structure", "iterstruct"]
-
+__all__ = ["Structure", "iterstruct", "config"]
 __version__ = "0.0.0"
 
 # disable security preventing DoS attacks with huge files
 etree.set_default_parser(etree.ETCompatXMLParser(huge_tree=True))
+
+STRUCTS = None
 
 
 class Structure():
     """A structure extracted from a vertical.
 
     """
-    def __init__(self, raw_vert):
-        self.raw = raw_vert
-        self.structs = set()
-        # for anything that looks pretty much like an opening tag
-        open_tags = re.compile(r"^<(\w+).*?>$", flags=re.M)
-        for match in open_tags.finditer(raw_vert):
-            struct = match.group(1)
-            offset = match.end()
-            close_tag = re.compile(r"^</{}.*?>$".format(struct), flags=re.M)
-            # if the closing counterpart is present somewhere down the line
-            if close_tag.search(raw_vert, offset):
-                # let's consider it a valid structure name
-                self.structs.add(struct)
-        self._first_line = self.raw.split("\n", maxsplit=1)[0]
-        self.name = re.findall(r"\w+", self._first_line)[0]
-        self.attr = dict(re.findall(r'(\w+)="([^"]+)"', self._first_line))
+    def __init__(self, raw_vert, structs):
+        self.raw = raw_vert.strip() + "\n"
+        self.structs = structs
+        first_line = self.raw.split("\n", maxsplit=1)[0]
+        self.name = re.search(r"\w+", first_line).group()
+        self.attr = dict(re.findall(r'(\w+)="(.*?)"', first_line))
 
     @lazy
     def xml(self):
@@ -42,7 +34,9 @@ class Structure():
         """
         xml = self._xmlize()
         try:
-            return etree.fromstring(xml)
+            xml = etree.fromstring(xml)
+            xml.tail = "\n"
+            return xml
         except etree.XMLSyntaxError as e:
             with NamedTempFile(mode="w", suffix=".xml", delete=False) as fh:
                 fh.write(xml)
@@ -51,7 +45,7 @@ class Structure():
                  "It has been dumped to {} for inspection.".format(fh.name)
             raise Exception(e)
 
-    def chunk(self, child, name, minmax):
+    def chunk(self, child, name, minmax, fallback_orig_id=None):
         """Split the structure into chunks of a given size.
 
         :param name: The name to give to the XML element representing the
@@ -63,6 +57,8 @@ class Structure():
         :param minmax: The length range the individual chunks should roughly
             fall into.
         :type minmax: (int, int)
+        :param fallback_orig_id: If structure has no @id attribute, this will
+            be used instead to generate the @ids of the chunks.
         :rtype: etree.Element
 
         """
@@ -82,12 +78,11 @@ class Structure():
                     return "end"
 
         root = etree.Element(self.xml.tag, attrib=self.xml.attrib)
-        root.text = "\n"
+        root.text = root.tail = "\n"
 
         def loop_vars(name, attrib, minmax):
             chunk = etree.Element(name, attrib=attrib)
-            chunk.text = "\n"
-            chunk.tail = "\n"
+            chunk.text = chunk.tail = "\n"
             chunk_length = random.randint(*minmax)
             return chunk, chunk_length, 0
 
@@ -122,30 +117,40 @@ class Structure():
         # annotate chunks with metadata
         chunk_count = len(root)
         for i, chunk in enumerate(root):
-            orig_id = chunk.get("id", None)
+            orig_id = chunk.get("id", fallback_orig_id)
+            if orig_id is None:
+                raise RuntimeWarning(
+                    "Original structure has no @id attribute, the @id attributes "
+                    "of groups under it might therefore not be unique. Specify a "
+                    "``fallback_root_id`` to bypass the issue.")
             chunk.set(root.tag + "_id", orig_id)
             chunk.set("id", "{}_{}".format(orig_id, i))
             chunk.set("position_in_text", chunk_pos(i, chunk_count))
 
         return root
 
-    def group(self, target, attr, as_struct, fallback_root_id=None):
+    def group(self, target, attr, as_struct, fallback_root_id=False):
         """Group target structures under the root according to attribute values.
 
         :param target: The structures to group.
         :param attr: Iterable of attributes by whose values to group them by.
         :param as_struct: The tag name to use for the groups.
+        :param fallback_root_id: If structure has no @id attribute, this will
+            be used instead to generate the @ids of the groups. If None, the
+            @ids will consist solely of the grouping attributes and it is the
+            user's responsibility to ensure they're unique.
         :rtype: etree.Element
 
         """
         root = etree.Element(self.xml.tag, attrib=self.xml.attrib)
-        root.text = "\n"
+        root.text = root.tail = "\n"
         root_id = root.get("id", fallback_root_id)
-        if root_id is None:
+        if root_id is False:
             raise RuntimeWarning(
                 "Parent structure has no @id attribute, the @id attributes of "
                 "groups under it might therefore not be unique. Specify a "
-                "``fallback_root_id`` to bypass the issue.")
+                "``fallback_root_id`` to bypass the issue, or set it to None "
+                "if uniqueness is ensured otherwise.")
 
         def new_group(attrib, id):
             g = etree.SubElement(root, as_struct, attrib=attrib)
@@ -156,7 +161,9 @@ class Structure():
         groups = {}
         for target in self.xml.iter(target):
             t_val = tuple(target.get(a, None) for a in attr)
-            id = root_id + "/" + ",".join(t_val)
+            id = ",".join(map(str, t_val))
+            if fallback_root_id is not None:
+                id = root_id + "/" + id
             if t_val not in groups:
                 # new Python 3.5 syntax; in case of dupes, the last occurrence
                 # of a key takes precedence:
@@ -207,27 +214,108 @@ class Structure():
         return vert
 
 
-def iterstruct(vert_file, struct=None):
+class ValidTags:
+    """Keep track of valid tag names in a vertical file.
+
+    """
+    def __init__(self, start=None, end=None, void=None, structs=None):
+        self.start = start if start else re.compile(r"<(\w+).*?(?<!/)>")
+        self.end = end if end else re.compile(r"</(\w+)>")
+        self.void = void if void else re.compile(r"<(\w+).*?/>")
+        self.stags = set()
+        self.etags = set()
+        self.vtags = set()
+
+    def add(self, line):
+        s = self.start.fullmatch(line)
+        e = self.end.fullmatch(line)
+        v = self.void.fullmatch(line)
+        if s:
+            self.stags.add(s.group(1))
+        elif e:
+            self.etags.add(e.group(1))
+        elif v:
+            self.vtags.add(v.group(1))
+        return bool(s or e or v)
+
+    def resolve(self):
+        return self.stags.intersection(self.etags).union(self.vtags)
+
+
+class DummyValidTags:
+    """Anamorphous to ValidTags but returns structs passed to constructor on
+    self.resolve().
+
+    """
+    def __init__(self, structs):
+        self.structs = structs
+
+    def add(self, line):
+        pass
+
+    def resolve(self):
+        return self.structs
+
+
+def iterstruct(vert_file, struct=None, structs=None):
     """Yield input vertical one struct at a time.
 
     :param vert_file: Input vertical.
     :param struct: The name of the struct into which the vertical will be
-        chopped. If None, the whole vertical is returned.
+        chopped. If None, the whole vertical is returned, wrapped in a
+        ``<root/>`` element.
+    :param structs: A set of tag names to be considered as valid nested
+        structures under ``struct``. When in doubt, leave ``None`` (automatic
+        discovery), otherwise those you missed might be XML-escaped.
     :rtype: Structure
 
     """
-    if struct is None:
-        yield Structure(vert_file.read())
+    # override structs with global STRUCTS if they aren't set (STRUCTS in turn
+    # might not be set, in which case this is a no-op)
+    if structs is None:
+        structs = STRUCTS
+
+    # if the whole input vertical is to be wrapped and structs were provided,
+    # we can take a shortcut
+    if struct is None and structs:
+        structs.add("root")
+        yield Structure("<root>\n" + vert_file.read().strip() + "\n</root>", structs)
         raise StopIteration
+    # else, we'll just surround the vertical with <root/> tags and go the
+    # regular way (line by line)
+    elif struct is None:
+        struct = "root"
+        vert_file = chain(["<root>"], vert_file, ["</root>"])
+
+    # NOTE: string concatenation inside a for-loop is supposedly slow in
+    # python, but building and then joining lists was comparably slow (mostly
+    # even slower); unless there's a point where it starts to make a big
+    # difference (in terms of the number of concatenations / length of the list
+    # required), there's no real incentive to change this code
     buffer = ""
+    structs = DummyValidTags(structs) if structs else ValidTags()
+    start = re.compile(r"<{}.*?>".format(struct))
+    end = re.compile(r"</{}>".format(struct))
     for line in vert_file:
+        line = line.strip()
         # if the buffer already contains something or if the current line
         # starts with the given structure name, then we're inside a target
         # structure that we want to collect; otherwise, just skip to the next
         # line
-        if not (buffer or line.startswith("<{}".format(struct))):
-            continue
-        buffer += line
-        if line.startswith("</{}".format(struct)):
-            yield Structure(buffer)
-            buffer = ""
+        if buffer or start.fullmatch(line):
+            structs.add(line)
+            buffer += line + "\n"
+            if end.fullmatch(line):
+                yield Structure(buffer, structs.resolve())
+                # NOTE: it might be a good idea to reset structs to a new
+                # ValidTags object at this point, if we truly want to allow for
+                # the possibility that different structures in the same
+                # vertical might allow different nested substructures; in
+                # practice though, indexing tools like manatee have just one
+                # set of valid tag names per vertical
+                buffer = ""
+
+
+def config(**kwargs):
+    for k, v in kwargs.items():
+        globals()[k.upper()] = v
